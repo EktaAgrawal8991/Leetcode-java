44238c58b170139c9797772458c9a89a9785b3ff
Ekta Agrawal <103818252+EktaAgrawal8991@users.noreply.github.com>
2024-01-23 20:41:43 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,8 @@
+/* 1. Two Sum
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * Ekta Agrawal
+ */
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
