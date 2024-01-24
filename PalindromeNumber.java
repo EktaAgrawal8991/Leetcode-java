f9710df0e71417c8daa9810b8ec58c6e508a09d4
Ekta Agrawal <103818252+EktaAgrawal8991@users.noreply.github.com>
2024-01-24 16:53:35 +0530

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,3 +1,8 @@
+/* 9. Palindrome Number
+ * Given an integer x, return true if x is a palindrome and false otherwise.
+ * Ekta Agrawal
+ */
+
 class Solution {
     public boolean isPalindrome(int x) {
         if (x<0){
